5864a0e043f61b6f2e4e6b503fbfe00cbad9ab6d
Akshat42 <akshatdivya03@gmail.com>
2021-03-30 15:15:35 +0530

    Add maximum subarray - brue force solution

---
+++ Divide And Conqure Algorithms/MaximumSubarrayNaive.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class MaximumSubarrayNaive {
+  static int MSN(int a[], int n) {
+		  String s = "";
+    int max = Integer.MIN_VALUE;
+    for (int i = 0; i < a.length; i++) {
+      for (int j = i; j < a.length; j++) {
+        int sum = findSum(a, i, j);
+        if (sum > max) { 
+				max = sum;
+				s = i+" "+j;
+		}
+      }
+    }
+	System.out.println(s);
+    return max;
+  }
+
+  static int findSum(int a[], int i, int j) {
+    int sum = 0;
+    for (int k = i; k <= j; k++) {
+      sum = sum + a[k];
+    }
+    return sum;
+  }
+
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int a[] = new int[n];
+    for (int i = 0; i < n; i++) {
+      a[i] = sc.nextInt();
+    }
+    System.out.println(MSN(a, n));
+  }
+}
