71dce336feea4168eab8f185710dd0aadeaa3255
Akshat42 <akshatdivya03@gmail.com>
2021-04-01 17:21:10 +0530

    reduce the complexity form O(n3) to O(n2)
    by re-using the previously calculated sum i.e, adding the current element to existing sum.

--- Divide And Conqure Algorithms/MaximumSubarrayNaive.java
+++ Divide And Conqure Algorithms/MaximumSubarrayNaive.java
@@ -2,28 +2,31 @@ import java.util.Scanner;
 
 class MaximumSubarrayNaive {
   static int MSN(int a[], int n) {
+		  int sum = 0;
 		  String s = "";
     int max = Integer.MIN_VALUE;
     for (int i = 0; i < a.length; i++) {
       for (int j = i; j < a.length; j++) {
-        int sum = findSum(a, i, j);
-        if (sum > max) { 
+			  sum = sum + a[j];
+        if (sum > max) {
+			   s = i+" "+j;	
 				max = sum;
-				s = i+" "+j;
 		}
+
       }
+	  sum = 0;
     }
 	System.out.println(s);
     return max;
   }
 
-  static int findSum(int a[], int i, int j) {
-    int sum = 0;
-    for (int k = i; k <= j; k++) {
-      sum = sum + a[k];
-    }
-    return sum;
-  }
+//  static int findSum(int a[], int i, int j) {
+//    int sum = 0;
+//    for (int k = i; k <= j; k++) {
+//      sum = sum + a[k];
+//    }
+//    return sum;
+//  }
 
   public static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
