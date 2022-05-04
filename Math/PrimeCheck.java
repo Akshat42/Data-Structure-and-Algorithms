0f5d52f426788831de53c3ce3f63fa61d51947e3
Akshat42 <akshatdivya03@gmail.com>
2022-05-04 18:35:00 +0530

    additional optimization for prime numbers

--- Math/PrimeCheck.java
+++ Math/PrimeCheck.java
@@ -4,7 +4,7 @@ class PrimeCheck {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        System.out.println(checkPrimeEffieient(n));
+        System.out.println(checkPrimeEffieient2(n));
     }
 
     // Time complexity O(n)
@@ -28,10 +28,10 @@ class PrimeCheck {
     // more effectent solution
     // with extra check for divisibility with 2 and 3 we can save many more iterations
     // it is almost 3 times faster that the previous effecient solution
-    static boolean checkPrimeEffieient(int n) {
+    static boolean checkPrimeEffieient2(int n) {
         if (n == 1) return false;
-        if (n == 1 || n == 3) return true;
-        if (n % 2 == 0 || n % 3 == 0) return false
+        if (n == 2 || n == 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
         for (int i = 5; i <= Math.sqrt(n); i = i + 6) {
             if (n % i == 0 || n % (i + 2) == 0) return false;
         }
